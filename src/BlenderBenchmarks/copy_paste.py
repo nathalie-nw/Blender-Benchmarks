f5158aefe579c5b365d2b309666969c4b4cc48b7
Nathalie Winkler <winkler.nathalie@gmx.net>
2025-10-14 21:44:38 +0200

    added copy paste for 4.2

--- src/BlenderBenchmarks/copy_paste.py
+++ src/BlenderBenchmarks/copy_paste.py
@@ -8,38 +8,79 @@ frames_copy = 5
 frames_to_copy = frames / frames_copy
 target_frame = 1
 
+use_gpencil_type = "GPENCIL" if bpy.app.version < (4, 5, 0) else "GREASEPENCIL"
+
 obj = bpy.context.object
-if not obj or obj.type != "GREASEPENCIL":
-    raise Exception("Select a Grease Pencil object first.")
+# blender 4.2
+if use_gpencil_type == "GPENCIL":
+    if not obj or obj.type != "GPENCIL":
+        raise Exception("Select a Grease Pencil object first.")
 
-# Go to source frame
-bpy.context.scene.frame_set(source_frame)
-bpy.ops.object.mode_set(mode="EDIT")
+    # Go to source frame
+    bpy.context.scene.frame_set(source_frame)
+    bpy.ops.object.mode_set(mode="EDIT_GPENCIL")
 
-# select and copy strokes
-bpy.ops.grease_pencil.select_all()
-bpy.ops.grease_pencil.copy()
-# print(bpy.ops.grease_pencil.copy)
+    bpy.context.area.type = "VIEW_3D"
 
-for f in range(frames_copy):
-    target_frame += frames_to_copy
-    target_frame = int(target_frame)
+    # select and copy strokes
+    bpy.ops.gpencil.select_all(action="TOGGLE")
+    bpy.ops.gpencil.copy()
 
-    #  go to target frame
-    bpy.ops.object.mode_set(mode="OBJECT")
-    bpy.context.scene.frame_set(target_frame)
-    print(target_frame)
-    # add blank frame
+    for f in range(frames_copy):
+        target_frame += frames_to_copy
+        target_frame = int(target_frame)
 
-    gp = bpy.context.object.data
-    layer = gp.layers.active
-
-    # check if frame already exists
-    if not any(f.frame_number == target_frame for f in layer.frames):
+        #  go to target frame
+        bpy.ops.object.mode_set(mode="OBJECT")
         bpy.context.scene.frame_set(target_frame)
-        bpy.ops.grease_pencil.insert_blank_frame()
+        print(target_frame)
 
-    # past strokes
-    layer = obj.data.layers.active
+        bpy.ops.object.mode_set(mode="EDIT_GPENCIL")
+        gp = bpy.context.object.data
+        print(bpy.context.object.data)
+        layer = gp.layers.active
+
+        # add frame
+        if not any(f.frame_number == target_frame for f in layer.frames):
+            layer.frames.new(target_frame)
+
+        # past strokes
+        layer = obj.data.layers.active
+        bpy.ops.gpencil.paste()
+        bpy.ops.object.mode_set(mode="OBJECT")
+
+# blender 4.5
+elif use_gpencil_type == "GREASEPENCIL":
+    if not obj or obj.type != "GREASEPENCIL":
+        raise Exception("Select a Grease Pencil object first.")
+
+    # Go to source frame
+    bpy.context.scene.frame_set(source_frame)
     bpy.ops.object.mode_set(mode="EDIT")
-    bpy.ops.grease_pencil.paste()
+
+    # select and copy strokes
+    bpy.ops.grease_pencil.select_all()
+    bpy.ops.grease_pencil.copy()
+
+    for f in range(frames_copy):
+        target_frame += frames_to_copy
+        target_frame = int(target_frame)
+
+        #  go to target frame
+        bpy.ops.object.mode_set(mode="OBJECT")
+        bpy.context.scene.frame_set(target_frame)
+        print(target_frame)
+
+        gp = bpy.context.object.data
+        layer = gp.layers.active
+
+        # add frame
+        if not any(f.frame_number == target_frame for f in layer.frames):
+            bpy.context.scene.frame_set(target_frame)
+            bpy.ops.grease_pencil.insert_blank_frame()
+
+        # past strokes
+        layer = obj.data.layers.active
+        bpy.ops.object.mode_set(mode="EDIT")
+        bpy.ops.grease_pencil.paste()
+        bpy.ops.object.mode_set(mode="OBJECT")
