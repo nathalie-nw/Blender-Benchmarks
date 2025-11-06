c0df8c21fa5b2402262f280eadab6ba941ec3376
Nathalie Winkler <winkler.nathalie@gmx.net>
2025-11-06 09:44:16 +0100

    check context

--- src/BlenderBenchmarks/measure_undo_time.py
+++ src/BlenderBenchmarks/measure_undo_time.py
@@ -6,17 +6,18 @@ import bpy
 start_time = time.time()
 
 
-# Check if in Draw Mode
-if bpy.context.mode in {"PAINT_GREASE_PENCIL", "PAINT_GPENCIL"}:
-    bpy.ops.ed.undo()
-    print(
-        "\nfile:",
-        os.path.basename(bpy.data.filepath),
-        "\nundo time:",
-        time.time() - start_time,
-    )
+if bpy.context.mode not in {'PAINT_GREASE_PENCIL', 'PAINT_GPENCIL'}:
+    if bpy.app.version < (4, 3, 0):
+        bpy.ops.object.mode_set(mode="PAINT_GPENCIL")
+    else:
+        bpy.ops.object.mode_set(mode="PAINT_GREASE_PENCIL")
 
 
-else:
-    bpy.context.mode
-    print("Not in Grease Pencil draw mode, undo skipped.")
+# Undo
+bpy.ops.ed.undo()
+print(
+    "\nfile:",
+    os.path.basename(bpy.data.filepath),
+    "\nundo time:",
+    time.time() - start_time,
+)
